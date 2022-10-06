4e7d49abbfdc9f5b80b6b7d66aa9276c0fabe547
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-10-06 16:43:13 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let x = 0
+let y = 0
+for (let index = 0; index < 5; index++) {
+    if (y % 2 == 0) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(100)
+        }
+        y += 1
+    } else {
+        x = 4
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            x += -1
+            basic.pause(100)
+        }
+        y += 1
+    }
+}
